1be3e02e69f3c3e59463d9e69ec2ef5612500e28
Hugo Rosnet <hugo.rosnet@cycloid.io>
2020-04-26 22:13:12 +0200

    tutorial: is working okay, but need div fixed

--- app/src/components/Images.js
+++ app/src/components/Images.js
@@ -1,44 +1,26 @@
 export class Image {
-  name;
-  src;
-  hidden;
-  hover; // not set
-  alt;
-  sound;
-  phoneme;
-
   constructor(name, srcPath, hidden, alt, soundFile, phoneme) {
-    this.name = name;
-    this.src = srcPath;
-    this.hidden = hidden;
-    this.alt = alt;
-    this.sound = new Audio(soundFile);
-    this.sound.type = 'audio/mp3';
-    this.phoneme = phoneme;
-  }
+    var that = this;
 
-  getName() {return this.name}
-  getSrc() {return this.src}
-  getHidden() {return this.hidden}
-  getHover() {return this.hover}
-  getAlt() {return this.alt}
-  getSound() {return this.sound}
-  getPhoneme() {return this.phoneme}
+    that.name = name;
+    that.src = srcPath;
+    that.hidden = hidden;
+    that.alt = alt;
+    that.sound = new Audio(soundFile);
+    that.sound.type = 'audio/mp3';
+    that.phoneme = phoneme;
 
-  setName(name) {this.name = name}
-  setSrc(src) {this.src = src}
-  setHidden(hidden) {this.hidden = hidden}
-  setHover(hover) {this.hover = hover}
-  setAlt(alt) {this.alt = alt}
-  setSound(sound) {this.sound = sound}
-  setPhoneme(phoneme) {this.phoneme = phoneme}
-
-  play() {
-    this.sound.play()
+    // The method need to be declared here
+    // because otherwise, it is not accessible as:
+    // "this" isn't the same after creation, and saving
+    // it doesn't seem to work
+    that.playSound = function() {
+      that.sound.play();
+    }
   }
 
   containsPhoneme(p) {
-    return p === this.phoneme
+    return p === that.phoneme;
   }
 }
 
@@ -46,8 +28,8 @@ export class Images {
 
   constructor() {
     var m = new Map();
-    const soundPath = "/statics/audio/"
-    const imagePath = "/statics/images/numbers/"
+    const soundPath = "statics/audio/"
+    const imagePath = "statics/images/numbers/"
 
     m.set("1",  Image.New(imagePath + "one.png", false, "image-one.png", soundPath + "good-notif.mp3", "e"));
     m.set("2",  Image.New(imagePath + "two.png", false, "image-two.png", soundPath + "negativ-notif.mp3", "t"));
--- app/src/pages/Tutorial.vue
+++ app/src/pages/Tutorial.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="tutorial q-pa-md">
     <div class="main">
-      <div class="placement" v-for="image in images">
-        <img class="image" v-bind:src=image.src v-bind:alt=image.alt>
+      <!-- When the image disappear the other div move, it should remain fixed -->
+      <div class="placement" v-for="(image, index) in images">
+        <img v-bind:id="'img' + image.name"
+            class="img"
+            v-if="!image.hidden"
+            v-bind:src="image.src"
+            v-bind:alt="image.alt"
+            @mouseover="image.playSound()"
+            @click="click(image)"
+        >
       </div>
     </div>
   </div>
@@ -13,11 +21,13 @@
 
 .main {
   text-align: center;
-  border: 1px solid #ff0000;
+  overflow: hidden;
+/*  border: 1px solid #ff0000;*/
 }
 
-.image {
+.img {
   border: 1px solid #0a0300;
+
 }
 
 .tutorial {
@@ -26,8 +36,8 @@
 }
 
 .placement {
-  display: inline-block;
   border: 1px solid #0000ff;
+  display: inline-block;
   margin-left: auto;
   margin-right: auto;
   padding: 130px;
@@ -51,24 +61,57 @@ import { Image } from '../components/Images'
 export default {
   name: 'Tutorial',
   methods: {
-    mouseOver() {
-      console.log("I'm OVER 1!")
-      success.play();
+    click(i) {
+      console.log("PARAMS: ", i)
+      console.log("PHONEME: ", i.phoneme)
+      console.log("SEARCHING: ", this.searching)
+      console.log("EVENT: ", event.target.id)
+
+      if (i.phoneme === this.searching) {
+        this.animateCSS('#' + event.target.id, "fadeOutLeft", function () { i.hidden = true})
+        console.log("clicked: ", i.name)
+      }
     },
-	},
-	mounted() {
-    const soundPath = "/statics/audio/"
-    const imagePath = "/statics/images/numbers/"
+    animateCSS(element, animationName, callback) {
+      const node = document.querySelector(element)
+      const ans = animationName.split(' ');
+
+      node.classList.add('animated')
+      for (var i = 0, length = ans.length; i < length; i++) {
+        node.classList.add(ans[i]);
+      }
+      function handleAnimationEnd() {
+          node.classList.remove('animated')
+          for (var i = 0, length = ans.length; i < length; i++) {
+            node.classList.remove(ans[i]);
+          }
+          node.removeEventListener('animationend', handleAnimationEnd)
+
+          if (typeof callback === 'function') callback()
+      }
+      node.addEventListener('animationend', handleAnimationEnd)
+    }
+  },
+	created() {
+    const soundPath = "statics/audio/";
+    const imagePath = "statics/images/numbers/";
+    const phoneme = "e"
 
     let img1 = new Image("1", imagePath + "one.png", false, "image-one.png", soundPath + "good-notif.mp3", "e");
-    let img2 = new Image("2", imagePath + "two.png", false, "image-two.png", soundPath + "negativ-notif.mp3", "t");
+    let img2 = new Image("2", imagePath + "two.png", false, "image-two.png", soundPath + "negative-notif.mp3", "t");
 
+    this.searching = phoneme;
     this.images = [
       img1,
       img2,
-    ]
+    ];
   },
-  data () { return {images: this.images}}
+ 	mounted() {
+    this.animateCSS("#img1", "fadeInRightBig")
+    this.animateCSS("#img2", "fadeInRightBig")
+  },
+ data () {
+   return {images: this.images}}
 }
 
 </script>
