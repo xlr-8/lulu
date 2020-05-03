2c5b69bdbb16ce535f230f21ed27a2febd6a3f22
Hugo Rosnet <hugo.rosnet@cycloid.io>
2020-05-03 21:47:31 +0200

    app: rename app frontend

---
+++ frontend/http.py
@@ -0,0 +1,36 @@
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import SocketServer
+import json
+import cgi
+
+PORT=8000
+
+class Server(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+        
+    def do_HEAD(self):
+        self._set_headers()
+        
+    # GET sends back a Hello world message
+    def do_GET(self):
+        self._set_headers()
+        self.wfile.write(json.dumps({'message': 'Hello API'}))
+        
+def run(server_class=HTTPServer, handler_class=Server, port=PORT):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    
+    print 'Starting httpd on port %d...' % port
+    httpd.serve_forever()
+    
+if __name__ == "__main__":
+    from sys import argv
+    
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
